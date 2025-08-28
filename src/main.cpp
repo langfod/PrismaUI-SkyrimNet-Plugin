3f9c38023f18fc3d513b560ba51f22b65e76723d
langfod <langfod@gmail.com>
2025-08-28 02:02:48 -0400

    fix after merge

--- src/main.cpp
+++ src/main.cpp
@@ -78,6 +78,8 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_s
         return false;
     }
 
+    logger::info("{} v{}"sv, Plugin::NAME, Plugin::VERSION.string());
+
     SKSE::Init(a_skse);
     SKSE::AllocTrampoline(1 << 10);
 
